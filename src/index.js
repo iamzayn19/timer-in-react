ee670fc40fa2fd85ca9373f97b5a1d2fb7ffb0c0
zain <iamzayn19@gmail.com>
2022-08-23 18:24:47 +0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Timer extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { 
+      seconds: 0
+    }
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  clock()
+  {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount()
+  {
+    this.interval = setInterval(() => this.clock(), 1000);
+  }
+
+  componentWillUnmount()
+  {
+    clearInterval(this.interval);
+  }
+
+render (){
+  return(
+    <div>
+      Seconds: {this.state.seconds}
+    </div>
+  ); 
+}
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Timer />);
